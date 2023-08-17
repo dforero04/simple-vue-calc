52d098ddbfd00757c38ae865adc227eeebaa6236
Daniel Forero <daniel.w.forero.ctr@army.mil>
2023-08-17 17:49:53 -0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+  import CalcContainer from './components/CalcContainer.vue'
+
+</script>
+
+<template>
+  <h1>Simple Vue Calculator</h1>
+  
+  <CalcContainer/>
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/CalcContainer.vue
@@ -0,0 +1,9 @@
+<script setup>
+  import {ref} from 'vue';
+
+  const equation = ref('');
+</script>
+
+<template>
+  <input v-model="equation" placeholder="Enter calculation">
+</template>
