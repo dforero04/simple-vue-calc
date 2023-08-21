01c46679b9985a86e66d87a24857ab9e3d72cee6
Daniel Forero <dwforero4@gmail.com>
2023-08-21 16:58:46 -0500

    Basically done. Further learnings could be to style using custom CSS

--- src/components/CalcContainer.vue
+++ src/components/CalcContainer.vue
@@ -2,40 +2,26 @@
 import { ref } from 'vue'
 
 const inputString = ref('')
-const inputNumbers = ref([])
-const inputOperators = ref([])
+const equation = ref('')
 
 function handleClear() {
   inputString.value = ''
-  inputNumbers.value = [];
-  inputOperators.value = [];
+  equation.value = []
 }
 
-function handleNumbers(event) {
-  inputNumbers.value.push(event.target.value);
+function handleClick(event) {
+  equation.value += event.target.value
+  inputString.value += event.target.value
 }
 
-function handleOperators(event) {
-  inputOperators.value.push(event.target.value)
+function handleOperator(event) {
+  equation.value += event.target.value
+  inputString.value = ''
 }
 
 function handleEquals() {
-  console.log("numbers: ", inputNumbers.value);
-  console.log("operators: ", inputOperators.value);
-
-  if (inputOperators.value.length >= inputNumbers.value.length) {
-    alert('Equation cannot end with an operator.');
-  } else if (inputNumbers.value.length === 1) {
-    inputString.value = inputNumbers.value;
-  } else {
-    let answer = Number(inputNumbers.value[0]);
-    for (let i = 1; i < inputNumbers.value.length; i++) {
-      if (inputOperators.value[i - 1] === '+') {
-        answer += Number(inputNumbers.value[i])
-      }
-    }
-    inputString.value = answer;
-  }
+  console.log(eval(equation.value));
+  inputString.value = eval(equation.value)
 }
 </script>
 
@@ -43,16 +29,26 @@ function handleEquals() {
   <div class="calc">
     <span class="title">Simple Vue Calculator</span>
     <div class="calc-container">
-      <input class="calc-input" v-model="inputString" placeholder="Enter calculation" />
+      <input class="number-input" v-model="inputString" />
       <div class="keypad">
-        <button @click="handleClear">Clear</button>
-        <button value="1" @click="handleNumbers">1</button>
-        <button value="2" @click="handleNumbers">2</button>
-        <button value="3" @click="handleNumbers">3</button>
-        <button value="4" @click="handleNumbers">4</button>
-        <button value="5" @click="handleNumbers">5</button>
-        <button value="+" @click="handleOperators">+</button>
-        <button @click="handleEquals">=</button>
+        <div class="numbers">
+          <button value="1" @click="handleClick">1</button>
+          <button value="2" @click="handleClick">2</button>
+          <button value="3" @click="handleClick">3</button>
+          <button value="+" @click="handleOperator">+</button>
+          <button value="4" @click="handleClick">4</button>
+          <button value="5" @click="handleClick">5</button>
+          <button value="6" @click="handleClick">6</button>
+          <button value="-" @click="handleOperator">-</button>
+          <button value="7" @click="handleClick">7</button>
+          <button value="8" @click="handleClick">8</button>
+          <button value="9" @click="handleClick">9</button>
+          <button value="*" @click="handleOperator">x</button>
+          <button value="0" @click="handleClick">0</button>
+          <button @click="handleClear">Clear</button>
+          <button value="/" @click="handleOperator">&#247;</button>
+          <button @click="handleEquals">=</button>
+        </div>
       </div>
     </div>
   </div>
@@ -65,7 +61,7 @@ function handleEquals() {
   position: relative;
 }
 
-.calc span.title {
+.title {
   font-weight: 700;
   letter-spacing: 2px;
   display: block;
@@ -82,19 +78,39 @@ input {
   color: blue;
   width: 80%;
   font-weight: 400;
-}
-
-input .calc-input {
   height: 55px;
   line-height: 55px;
   text-align: right;
+  margin: auto auto auto 10px;
   padding: 0 20px;
   border-radius: 10px;
 }
 
 .keypad {
+  width: 90%;
+  margin-left: 5%;
+  margin-top: 40px;
+}
+
+.numbers {
+  width: 100%;
+  font-size: 0;
+  text-align: center;
+}
+
+button {
+  font-size: 24px;
+  color: red;
+  display: inline-block;
+  width: 25%;
+  text-align: center;
+  margin-bottom: 25px;
+}
+
+.operators {
+  width: 100%;
   display: flex;
-  align-items: center;
-  justify-content: center;
+  text-align: right;
+  flex-direction: column;
 }
 </style>
